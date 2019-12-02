94ad7c9040f84e67d2134d7e3d4ade81bc68ebce
varshaekport <varsha.ekport@gmail.com>
2019-12-02 14:52:34 +0530

    ADD CANDIDATE MODEL

---
+++ models/candidate.js
@@ -0,0 +1,140 @@
+import { Schema, model } from "mongoose";
+
+const candidateSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: Text,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    website: {
+        type: String,
+        required: false
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            joining_date: {
+                type: Date,
+                required: true
+            },
+            last_date: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ], education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldOfStudy: {
+                type: String,
+                required: true
+            },
+            sta: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    is_email_verified: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+    is_contact_verified: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+    account_activated_on: {
+        type: Date
+    },
+    emailToken: String
+}, { timestamps: true });
+
+export default model("Candidate", candidateSchema);
